b905f1f6fa1a1c2cacfbf88062614ba7e1cf792b
Takuamru <yurimaru4731@gmail.com>
2023-12-23 17:10:08 +0900

    4-3 ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable,:rememberable
 
   # ニックネーム、メールアドレス、パスワードの必須チェック
   validates :nickname, :email, :password, presence: true
