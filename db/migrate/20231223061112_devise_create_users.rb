df0e78db07dafd75501268438a48e896f5a35f07
Takuamru <yurimaru4731@gmail.com>
2023-12-30 16:31:37 +0900

    4-9 マイグレーションファイルの修正

--- db/migrate/20231223061112_devise_create_users.rb
+++ db/migrate/20231223061112_devise_create_users.rb
@@ -8,8 +8,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :last_name_kana, null: false
       t.string :first_name_kana, null: false
       t.string :nickname, null: false
-      t.string :email, null: false, unique: true
-      t.string :encrypted_password, null: false
+      t.string :email, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
       t.date :birth_date, null: false
 
     ## Recoverable
@@ -21,5 +21,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
       t.timestamps null: false
     end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    
   end
 end
