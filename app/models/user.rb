b48db75332ec18e15da70e5a8b125bc9705e4fce
Takuamru <yurimaru4731@gmail.com>
2023-12-30 16:54:41 +0900

    4-10 テストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,10 @@ class User < ApplicationRecord
 
   # ニックネーム、メールアドレス、パスワードの必須チェック
   validates :nickname, :password, presence: true
-        
-  # メールアドレスの一意性と@の含有
-  validates :uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-        
+
   # パスワードの長さとフォーマット（6文字以上、半角英数字混合）
-  validates :password, length:, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
-        
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+
   # 本人情報確認のバリデーション
   validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
   validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
