d2aee6c6b353908cf8b492eec4aecc2c4b46b52e
Takuamru <yurimaru4731@gmail.com>
2024-01-06 16:12:29 +0900

    5-2 商品コントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
-    # Your code here (e.g., setting up instance variables for the view)
+
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_id, :shipping_area_id, :shipping_day_id, :image)
   end
 end
