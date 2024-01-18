19c7f05cec712d10dd88b2cf85b2d4e930cc2d8b
Takuamru <yurimaru4731@gmail.com>
2024-01-18 15:52:53 +0900

    9-4 レビュー箇所修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,12 +38,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user_id
-      @item.destroy
-      redirect_to root_path
-    else
-      redirect_to item_path(@item)
-    end
+    redirect_to root_path
   end  
 
   private
