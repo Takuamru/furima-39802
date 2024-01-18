8bf6836ef37bc20aaf0f0cc7dc4092ff8237d2f8
Takuamru <yurimaru4731@gmail.com>
2024-01-18 13:47:00 +0900

    8-1 コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_if_not_owner, only: [:edit, :update]
+
 
   def index
     @items = Item.all.order(created_at: :desc)  # 作成日時で並び替えた商品を取得
@@ -20,12 +23,31 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])  # 特定の商品を取得
   end
   
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_id, :shipping_area_id, :shipping_day_id, :image)
+  end
+
+  def redirect_if_not_owner
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
