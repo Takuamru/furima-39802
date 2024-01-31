3f71a5cebcc47d10c1f87430f12ff42f10e98187
Takuamru <yurimaru4731@gmail.com>
2024-02-01 08:47:17 +0900

    10-15 テストコード修正

--- spec/models/purchase_form_spec.rb
+++ spec/models/purchase_form_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe PurchaseForm, type: :model do
   before do
-    # 必要なデータの準備
-    @purchase_form = FactoryBot.build(:purchase_form)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item, user: user)
+    @purchase_form = FactoryBot.build(:purchase_form, user_id: user.id, item_id: item.id)
   end
 
   describe '商品購入' do
+    
     context '購入がうまくいくとき' do
       it '必要な情報が全て存在すれば購入できる' do
         expect(@purchase_form).to be_valid
@@ -19,6 +21,24 @@ RSpec.describe PurchaseForm, type: :model do
     end
 
     context '購入がうまくいかないとき' do
+      it 'user_idが空だと保存できない' do
+        @purchase_form.user_id = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("User can't be blank")
+      end
+  
+      it 'item_idが空だと保存できない' do
+        @purchase_form.item_id = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Item can't be blank")
+      end
+  
+      it 'tokenが空だと保存できない' do
+        @purchase_form.token = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Token can't be blank")
+      end
+      
       it '郵便番号が空だと購入できない' do
         @purchase_form.postal_code = ''
         @purchase_form.valid?
@@ -63,6 +83,12 @@ RSpec.describe PurchaseForm, type: :model do
 
       it '電話番号が英数混合では購入できない' do
         @purchase_form.phone_number = 'abcd1234567'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Phone number is invalid")
+      end
+
+      it '電話番号が9桁以下では購入できない' do
+        @purchase_form.phone_number = '123456789' # 9桁の電話番号
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include("Phone number is invalid")
       end
